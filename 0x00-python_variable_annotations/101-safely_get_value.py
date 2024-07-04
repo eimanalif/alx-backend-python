878127ec2d83cea443d0442a9fdd4e0b453e3824
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-07-04 18:42:54 +0000

    task 102

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-''' Given the parameters and the return values, add type annotations to the function
+''' Given the parameters and the return values,
+    add type annotations to the function
 def safely_get_value(dct, key, default = None):
     if key in dct:
         return dct[key]
