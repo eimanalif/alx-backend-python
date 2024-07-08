9f5b31aaedffd406b5d8f1fc3cd3bb83994540cf
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-07-08 12:18:58 +0000

    task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    '''Waits for random number of seconds.
+    '''
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
---
+++ 0x01-python_async_function/README.md
@@ -0,0 +1,2 @@
+0x01-python_async_function
+a set of tasks about async - await concept
