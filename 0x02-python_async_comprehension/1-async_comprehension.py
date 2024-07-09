ed5f50eaa4e39d5ff2701db899f58c88f8c9d909
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-07-09 18:48:21 +0000

    task 2

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,7 +3,9 @@
 import asyncio
 from typing import List
 
+
 async_generator = __import__('0-async_generator').async_generator
+
 
 async def async_comprehension() -> List[float]:
     '''Creates a list of 10 numbers'''
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''task02'''
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    '''Executes async_comprehension 4 times'''
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    return time.time()- start_time
